e322f61dc5dc48ee8cafb3c6a9a8d5c961a371c5
kill081377 <lupomano@gmail.com>
2018-03-10 21:11:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const prefix = "-"
 
 client.on('ready', () => {
   console.log('client.user.setActivity('Phone', {type: 'WATCHING'});
-});
 
 client.on('message', message => {
   if (message.content == prefix + "test") {
