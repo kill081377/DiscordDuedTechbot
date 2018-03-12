c00339f239ba02094e6196e65a7d6c21cf022eb6
kill081377 <lupomano@gmail.com>
2018-03-12 16:37:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const prefix = ">";
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-   client.user.setPresence({ status: 'online', game: { name: '>help' } });
+   client.user.setGame(`${prefix} is the prefix!`);
 });
 
 client.on("guildCreate", guild => {
