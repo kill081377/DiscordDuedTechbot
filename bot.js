c4d1e927683b9a9cfad219653eaeb845369172d9
kill081377 <lupomano@gmail.com>
2018-03-18 09:17:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -120,6 +120,10 @@ if(!message.member.roles.some(r=>["Admin", "Mod", "Founder", "Owner", "Co-Owner"
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
 
+  if (command === "invite") {
+   message.channel.send("https://discordapp.com/oauth2/authorize?client_id=421669430074802176&scope=bot&permissions=8")
+  }
+  
   if (command === "math") {
    var numbers = [
     '1',
